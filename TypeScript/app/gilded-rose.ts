c850c6d8512d3dff68230ca8c9aea212bc05be69
“Lasantha <lasantha57@gmail.com>
2023-11-16 23:45:23 +1300

    refactor: remove old logic from updateQuality function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,50 +25,6 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
-      // if (item.name != ItemType.AgedBrie && item.name != ItemType.BackstagePasses) {
-      //   if (item.quality > 0) {
-      //     if (item.name != ItemType.Sulfuras) {
-      //       item.quality = item.quality - 1
-      //     }
-      //   }
-      // } else {
-      //   if (item.quality < 50) {
-      //     item.quality = item.quality + 1
-      //     if (item.name == ItemType.BackstagePasses) {
-      //       if (item.sellIn < 11) {
-      //         if (item.quality < 50) {
-      //           item.quality = item.quality + 1
-      //         }
-      //       }
-      //       if (item.sellIn < 6) {
-      //         if (item.quality < 50) {
-      //           item.quality = item.quality + 1
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-      // if (item.name != ItemType.Sulfuras) {
-      //   item.sellIn = item.sellIn - 1;
-      // }
-      // if (item.sellIn < 0) {
-      //   if (item.name != ItemType.AgedBrie) {
-      //     if (item.name != ItemType.BackstagePasses) {
-      //       if (item.quality > 0) {
-      //         if (item.name != ItemType.Sulfuras) {
-      //           item.quality = item.quality - 1
-      //         }
-      //       }
-      //     } else {
-      //       item.quality = item.quality - item.quality
-      //     }
-      //   } else {
-      //     if (item.quality < 50) {
-      //       item.quality = item.quality + 1
-      //     }
-      //   }
-      // }
-
       switch (item.name) {
         case ItemType.AgedBrie:
           this.updateAgedBrie(item);
@@ -109,7 +65,7 @@ export class GildedRose {
         item.quality += 1;
       }
     }
-    
+
     item.sellIn -= 1;
 
     if (item.sellIn < 0) {
